d687375f9b908cce5682dee1870263bb441cb599
the 14th web <dennis.itua1@gmail.com>
2019-12-05 23:34:12 -0500

    updating database connection

---
+++ movie_db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO movies (movie_name, watched) VALUES ('Frozen', FALSE);
+
+INSERT INTO movies (movie_name, watched) VALUES ('Titanic', FALSE);
+
+INSERT INTO movies (movie_name, watched) VALUES ('Men In Black', FALSE);
