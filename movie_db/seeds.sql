acdd301fef536ed9d8805ad6f8dda0d11a668052
the 14th web <dennis.itua1@gmail.com>
2019-12-06 23:47:38 -0500

    updating app folders and file

--- movie_db/seeds.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO movies (movie_name, watched) VALUES ('Frozen', FALSE);
-
-INSERT INTO movies (movie_name, watched) VALUES ('Titanic', FALSE);
-
-INSERT INTO movies (movie_name, watched) VALUES ('Men In Black', FALSE);
--- server.js
+++ server.js
@@ -1,42 +1,44 @@
 // Import dependencies.
-var express = require("express");
-var path = require("path");
-var exphbs = require("express-handlebars");
-var favicon = require("serve-favicon");
-var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
-
-var db = require("./models");
+var express = require('express');
+var path = require('path');
+var exphbs = require('express-handlebars');
+var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var db = require('./models');
 
 // Initialize app.
 var app = express();
 
-// Provides static content for the app from the PUBLIC directory
-app.use(express.static(path.join(__dirname, "public")));
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(path.join(__dirname, 'public')));
 
 var PORT = process.env.PORT || 1414;
 
 // Set handlebars as view engine.
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Favicon
-app.use(favicon(path.join(__dirname, "public/assets/images", "favicon.png")));
+// console.log("antes de favicon");
+// app.use(favicon(path.join(__dirname, 'public/assets/images', 'favicon.ico')));
+// console.log("después de favicon");
 
 // Body Parser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: true }));
+console.log("después de body-parser");
 
-// Method override
-app.use(methodOverride("_method"));
+// Method override.
+app.use(methodOverride('_method'));
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/sport_controller");
-app.use("/", routes);
+var routes = require('./controllers/movie_controller');
+app.use('/', routes);
 
-// Begin listening
-app.listen(PORT, function() {
-	console.log("App listening on PORT: " + PORT);
+// Start listening.
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+        console.log('listening on port ' + PORT);
+    });
 });
-
-
