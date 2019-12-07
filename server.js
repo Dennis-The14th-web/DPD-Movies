baf9a04fd957fa6e517617367badc877ba35260e
the 14th web <dennis.itua1@gmail.com>
2019-12-06 23:53:36 -0500

    Gitignore issue fixed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var path = require('path');
 var exphbs = require('express-handlebars');
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var db = require('./models');
