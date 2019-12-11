9525f80e80649ef9f1fb5a280854cbd9457ff63e
Dorian Bishop <dorianbishop427@gmail.com>
2019-12-10 20:25:03 -0500

    added code to handle bars, fixed some broken code

--- controllers/movie_controller.js
+++ controllers/movie_controller.js
@@ -44,14 +44,13 @@ router.get('/rating', function(req, res) {
     });
 });
 
-var queryUrl = import("./keys")
-var options = import("./keys")
+
 // POST route which calls Sequelize's create method with the movie name given.
 router.post('/api/new/movie', function(req, res) {
     
     var movieName = req.body.name;
 
-    var queryUrl = "http://omdbapi.com/?apikey=process.env.API_KEY&t=" + movieName;
+    var queryUrl = "http://omdbapi.com/?apikey=d69e1782&t=" + movieName;
     request(queryUrl, function(error, response, body) {
        
 
@@ -69,7 +68,7 @@ router.post('/api/new/movie', function(req, res) {
                 url: 'https://api.themoviedb.org/3/movie/' + imdbId + '/videos',
                 qs: {
                     language: 'en-US',
-                    api_key: 'process.env.api_key'
+                    api_key: 'd50548305ff81a83c1c65efa4ce59583'
                 },
                 body: '{}'
             };
